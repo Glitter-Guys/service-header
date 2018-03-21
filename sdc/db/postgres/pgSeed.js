3d200db2092e26110e5724455e750d1bec3a4621
Daniel Paikoff <dpaikoff@gmail.com>
2018-03-21 16:35:09 -0700

    add create index statements

--- sdc/db/postgres/pgSeed.js
+++ sdc/db/postgres/pgSeed.js
@@ -15,18 +15,40 @@ const cn = {
 
 const db = pgp(cn);
 
-insert.doMassiveInsert(db, 'groups', insert.groupsColumnList, insert.generateFakeGroupsRow, 1000)
-  .then(data => {
-    insert.doMassiveInsert(db, 'events', insert.eventsColumnList, insert.generateFakeEventsRow, 10000)
-      .then(data => {
-        console.log(data);
-      })
-      .catch(error => {
-        console.log(error);
-      });
-    console.log(data);
-  })
-  .catch(error => {
-    console.log(error);
-  });
+//insert.doMassiveInsert(db, 'groups', insert.groupsColumnList, insert.generateFakeGroupsRow, 1000)
+  //.then(data => {
+    //insert.doMassiveInsert(db, 'events', insert.eventsColumnList, insert.generateFakeEventsRow, 10000)
+      //.then(data => {
+        //console.log(data);
+        //buildIndexes(indexSQL);
+      //})
+      //.catch(error => {
+        //console.log(error);
+      //});
+    //console.log(data);
+  //})
+  //.catch(error => {
+    //console.log(error);
+  //});
 
+const indexSQL = `
+  CREATE UNIQUE INDEX eid_idx ON events (eid);
+  CREATE INDEX group_id_idx ON events (group_id);
+  CREATE UNIQUE INDEX gid_idx ON groups (gid);
+`;
+
+//const indexSQL = `
+  //CREATE INDEX eid_idx ON events USING hash (eid);
+  //CREATE INDEX group_id_idx ON events USING hash (group_id);
+  //CREATE INDEX gid_idx ON groups USING hash (gid);
+//`;
+
+const buildIndexes = (sql) => {
+  db.any(sql, [true])
+    .then(r => {
+      console.log(r);
+    })
+    .catch(err => {
+      throw err;
+    });
+};
