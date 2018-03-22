2c4aba8658d0bf587f3c38657f7e47e53fbacae5
Daniel Paikoff <dpaikoff@gmail.com>
2018-03-22 16:22:12 -0700

    Add dependencies, add dbHelpers, add server index.js

---
+++ sdc/server/index.js
@@ -0,0 +1,34 @@
+require('newrelic');
+const fs = require('fs');
+const path = require('path');
+const morgan = require('morgan');
+const express = require('express');
+const bodyParser = require('body-parser');
+const dbh = require('../db/postgres/dbHelpers.js');
+
+const app = express();
+
+// for logging
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+
+app.use(express.static(path.resolve(__dirname, '../client/public')));
+
+app.get('/events/:id', async (req, res) => {
+  const id = req.params.id;
+  const pool = dbh.pool;
+  const sql = dbh.selectToJson(id);
+  try {
+    const result = await dbh.runQuery(pool, sql);
+    res.send(JSON.stringify(result));
+  } catch(err) {
+    res.status(500);
+    res.send('An invalid ID was searched');
+  }
+});
+
+app.set('port', 8080);
+
+app.listen(app.get('port'), () => {
+  console.log('Listening to port ' + app.get('port'));
+});
